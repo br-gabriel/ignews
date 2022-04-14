f66ae5486848436530b1f524b9ebd23f51299440
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-04-14 18:47:37 -0300

    created subscription identifier

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -20,6 +20,43 @@ export default NextAuth({
     signingKey: process.env.SIGNING_KEY,
   },
   callbacks: {
+    async session({session}) {
+      try {
+        const userActiveSubscription = await fauna.query( 
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(
+                q.Index('subscription_by_status'),
+                'active'
+              )
+            ])
+          )
+        )
+        
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription
+        }
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null,
+        }
+      }
+      
+    },
     async signIn({user, account, profile}) {
       const { email } = user
 
@@ -53,23 +90,3 @@ export default NextAuth({
     }
   }
 })
-
-/*
-  callbacks: {
-    async signIn({user, account, profile}): Promise<boolean>{
-      const { email } = user
-      try {
-        await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            { data: { email } }
-          )
-        )
-        return true;
-      } catch {
-        return false;
-      }
-      
-    },
-  }
-*/
